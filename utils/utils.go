83d16267997fe0ca6e8edd2076f96adbe94e5b5a
DurininKirous <blackmetalchel@mail.ru>
2024-10-19 01:00:31 +0300

    ver 1

--- utils/utils.go
+++ utils/utils.go
@@ -10,14 +10,3 @@ func Contains(a string, slices []string) bool {
 	}
 	return res
 }
-
-func ContainsInt(b int, slices []int) bool {
-	var res bool = false
-	for _, slice := range slices {
-		if slice == b {
-			res = true
-			break
-		}
-	}
-	return res
-}
