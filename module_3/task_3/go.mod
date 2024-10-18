efd8f711cd8712f9873859b0237934e16402549a
DurininKirous <blackmetalchel@mail.ru>
2024-10-19 02:01:51 +0300

    ending of theory task_3

--- module_3/task_3/go.mod
+++ module_3/task_3/go.mod
@@ -1,3 +1,9 @@
 module task_3
 
 go 1.23.1
+
+require (
+	github.com/DurininKirous/utils v1.1.0
+	github.com/DurininKirous/utils/v2 v2.0.0
+	github.com/huandu/xstrings v1.5.0
+)
--- module_3/task_3/main.go
+++ module_3/task_3/main.go
@@ -7,15 +7,26 @@ import (
 	"task_3/wordz"
 
 	"github.com/DurininKirous/utils"
+	utilsV2 "github.com/DurininKirous/utils/v2"
 	"github.com/huandu/xstrings"
 )
 
 func main() {
+	isExistV2 := utilsV2.InSlice("two", wordz.Words)
+	if isExistV2 {
+		fmt.Println("Using utilsV2.InSlice and find value")
+	}
 	isExist := utils.Contains(wordz.Random(), wordz.Words)
 	if isExist {
 		fmt.Println("Slice Words contain finding value")
 	} else {
 		fmt.Println("Slice Words doesn't contain finding value")
+	}
+	isExistInt := utils.ContainsInt(5, []int{1, 2, 3, 4, 5})
+	if isExistInt {
+		fmt.Println("Slice Int containn finding value")
+	} else {
+		fmt.Println("Slice Int doesn't contain finding value")
 	}
 	fmt.Println(xstrings.Shuffle(durininkirous.City()), durininkirous.Digit())
 }
